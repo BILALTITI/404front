8b2bb8eae93ebad0512bd66507f942a1381138e2
Bilal Altiti <bilalaltiti@hotmail.com>
2026-04-17 00:49:37 +0300

    en

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,6 @@ import { routing } from "./src/i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ["/", "/(ar|en)/:path*"],
+  // Match all pathnames except API, Next internals, and static files (see next-intl middleware docs).
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
